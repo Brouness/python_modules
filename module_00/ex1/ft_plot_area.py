6a583e96b792fb4190355467b5db92900430f135
Youness Bourajli <happyworkyouness@gmail.com>
2026-03-03 17:44:48 +0000

    module 00

---
+++ module_00/ex1/ft_plot_area.py
@@ -0,0 +1,4 @@
+def ft_plot_area() ->None:
+	a = int(input("Enter length: "))
+	b = int(input("Enter width: "))
+	print("Plot area:", a * b)w
---
+++ module_00/ex2/ft_harvest_total.py
@@ -0,0 +1,7 @@
+def	ft_harvest_total() ->None:
+	a = int(input("Day 1 harvest: "))
+	b = int(input("Day 1 harvest: "))
+	c = int(input("Day 1 harvest: "))
+	print("Total harvest:", a + b + c)
+
+ft_harvest_total()
