0d8059539619d05c0a6cb8198944e2722cc0831e
Youness Bourajli <ybourajl@e3r7p1.1337.ma>
2026-03-13 13:10:37 +0000

    :

---
+++ Python_Module_01/ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+def ft_garden_intro(name: str, height: int, age: int) -> None:
+	print("=== Welcome to My Garden ===")
+	print("Plant: "+ name)
+	print("Height: "+ str(height) +"cm")
+	print("Age:", age, "days")
+	print("\n=== End of Program ===")
+
+if __name__ == "__main__":
+	ft_garden_intro("Rose", 25, 30)
---
+++ Python_Module_01/ex2/ft_plant_growth.py
@@ -0,0 +1,25 @@
+class Plant:
+	def __init__(self, name: str, height: int, age: int) -> None:
+		self.name = name
+		self.height = height
+		self.age = age
+	def grow(self) -> None:
+		self.height += 6
+	def age(self) -> None:
+		self.age += 6
+	def get_info(self) -> None:
+		return print(f"Growth this week: +{6}cm")
+
+def ft_plant_growth() -> None:
+	Rose = Plant("Rose", 25, 30)
+	print("=== Day 1 ===")
+	print(f"{Rose.name}: {Rose.height}cm, {Rose.age} days old")
+	print(f"=== Day 7 ===")
+	Rose.grow()
+	print(Rose.height)
+	Rose.age()
+	print(f"{Rose.name}: {Rose.height}cm, {Rose.age} days old")
+	Rose.get_info()
+
+if __name__ == "__main__":
+	ft_plant_growth()
