2fd870ae6e6fd7f6cdf8d51ce637b59910d886d3
Youness Bourajli <ybourajl@e3r7p1.1337.ma>
2026-03-14 14:42:49 +0000

    ex5

--- Python_Module_01/ex3/ft_plant_factory.py
+++ Python_Module_01/ex3/ft_plant_factory.py
@@ -1,20 +1,34 @@
 class Plant:
-	def __init__(self, name, height, age) -> None:
-		self.name = name
-		self.height = height
-		self.age = age
-		print(f"Created: {name} ({height}cm, {age} days)")
-	def grow(self) -> None:
-		self.height += 1
-	def age_grow(self) -> None:
-		self.age += 1
+    counter: int
+    counter = 0
+
+    def __init__(self, name: str, height: int, age: int) -> None:
+        Plant.counter += 1
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def grow(self) -> None:
+        self.height += 1
+
+    def age_grow(self) -> None:
+        self.age += 1
+
 
 def ft_plant_factory() -> None:
-	print("=== Plant Factory Output ===")
-	plant_data = [("Rose", 25, 30), ("Oak", 200, 365), ("Cactus", 5, 90),
-		("Sunflower", 80, 45), ("Fern", 15, 120)]
-	for data in plant_data:
-		Plant_add = Plant(data[0], data[1], data[2])
-	print(f"\nTotal plants created: {Plant_add.counter}")
+    print("=== Plant Factory Output ===")
+    plant_data = [
+        ("Rose", 25, 30),
+        ("Oak", 200, 365),
+        ("Cactus", 5, 90),
+        ("Sunflower", 80, 45),
+        ("Fern", 15, 120)
+        ]
+    for data in plant_data:
+        add = Plant(data[0], data[1], data[2])
+        print(f"Created: {add.name} ({add.height}cm, {add.age} days)")
+    print(f"\nTotal plants created: {Plant.counter}")
 
-ft_plant_factory()
+
+if __name__ == "__main__":
+    ft_plant_factory()
