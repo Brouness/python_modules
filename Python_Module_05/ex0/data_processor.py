229b0b06987dd90e51a1183d88da325bac3ad1aa
Youness Bourajli <happyworkyouness@gmail.com>
2026-04-16 15:41:37 +0100

    youness

--- Python_Module_05/ex0/data_processor.py
+++ Python_Module_05/ex0/data_processor.py
@@ -39,13 +39,16 @@ class NumericProcessor(DataProcessor):
             return False
 
     def ingest(self, data: Any) -> None:
-        if not self.validate(data):
-            raise ValueError("Invalid data !")
-        elif isinstance(data, list):
-            for element in data:
-                self._storage.append(str(element))
-        else:
-            self._storage.append(str(data))
+        try:
+            if not self.validate(data):
+                raise ValueError
+            elif isinstance(data, list):
+                for element in data:
+                    self._storage.append(str(element))
+            else:
+                self._storage.append(str(data))
+        except ValueError:
+            raise ValueError("Improper numeric data")
 
 
 class TextProcessor(DataProcessor):
@@ -100,22 +103,82 @@ class LogProcessor(DataProcessor):
             return False
 
     def ingest(self, data: Any) -> None:
-        if not self.validate(data):
-            raise ValueError("Invalid data!")
-        elif isinstance(data, dict):
-            for key, value in data.items():
-                my_list = key, value
-                self._storage.extend(my_list)
-        elif isinstance(data, list):
-            for element in data:
-                for key, value in element.items():
+        try:
+            if not self.validate(data):
+                raise ValueError("Invalid data!")
+            elif isinstance(data, dict):
+                for key, value in data.items():
                     my_list = key, value
                     self._storage.extend(my_list)
+            elif isinstance(data, list):
+                for element in data:
+                    for key, value in element.items():
+                        my_list = key, value
+                        self._storage.extend(my_list)
+        except ValueError:
+            raise ValueError("Improper numeric data")
 
 
-my_list = [{"amine": "bourajli", "hfdla": "fd"}, {"alo ": "fdjshf"}, {"fdf": "55"}]
-younes = LogProcessor()
-booll = younes.validate(my_list)
-print(booll)
-younes.ingest(my_list)
-print(younes._storage)
+def Testing_Numeric_data() -> None:
+    print("Testing Numeric Processor...")
+    Numeric = NumericProcessor()
+    print(f" Trying to validate input '42': {Numeric.validate(42)}")
+    hello = "Hello"
+    print(f" Trying to validate input {hello}: {Numeric.validate(hello)}")
+    print(" Test invalid ingestion of string 'foo' without prior validation:")
+    try:
+        Numeric.ingest("foo")
+    except ValueError as e:
+        print(f" Got exception: {e}")
+    my_list = [1, 2, 3, 4, 5]
+    Numeric.ingest(my_list)
+    print(f" Processing data: {my_list}")
+    print(" Extracting 3 values...")
+    index, value = Numeric.output()
+    print(f" Numeric value {index}: {value}")
+    index, value = Numeric.output()
+    print(f" Numeric value {index}: {value}")
+    index, value = Numeric.output()
+    print(f" Numeric value {index}: {value}")
+    print()
+
+
+def Testing_Text_Processor() -> None:
+    print("Testing Text Processor...")
+    Text = TextProcessor()
+    print(f" Trying to validate input '42': {Text.validate(42)}")
+    hello = ["Hello", "Nexus", "World"]
+    Text.ingest(hello)
+    print(f" Processing data: {hello}")
+    print(" Extracting 1 value...")
+    index, value = Text.output()
+    print(f" Numeric value {index}: {value}")
+    print()
+
+
+def Testing_Log_Processor() -> None:
+    print("Tesring Log Processor...")
+    Log = LogProcessor()
+    hello = "Hello"
+    print(f" Trying to validatr input {hello}: {Log.validate(hello)}")
+    my_list = [
+        {"log_level": "NOTICE", "log_message": "Connection to server"},
+        {"log_level": "ERROR", "log_message": "Unauthorized access!!"}
+        ]
+    Log.ingest(my_list)
+    print(f" Processing data: {my_list}")
+    print("Extracting 2 values...")
+    x, y = Log.output()
+    print(f"Log entry {x}: {y}")
+    x, y = Log.output()
+    print(f"Log entry {x}: {y}")
+
+
+def main() -> None:
+    print("=== Code Nexus - Data Processor ===\n")
+    Testing_Numeric_data()
+    Testing_Text_Processor()
+    Testing_Log_Processor()
+
+if __name__ == "__main__":
+    main()
